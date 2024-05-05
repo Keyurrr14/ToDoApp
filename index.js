2d37a851b9b810d1ea62927dba07b17214e9b9d4
Keyurrr14 <keyurrathod9920@gmail.com>
2024-05-05 13:01:18 +0530

    made dyanmic cards

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { log } = require("console");
 const app = express();
 
 app.use(express.json());
@@ -8,16 +10,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
-app.get("/profile/:username", (req, res) => {
-    res.send(`Welcome, ${req.params.username}`);
+app.post("/create", (req, res) => {
+  fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.description, (err) => {
+    res.redirect("/")
+  });
 });
 
-
-app.get("/author/:username/:age", (req, res) => {
-    res.send(`Welcome, ${req.params.username} ${req.params.age}`);
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", {filename : req.params.filename, filedata: filedata});
+  })
 });
 
 app.listen(3000);
